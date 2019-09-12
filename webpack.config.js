3de74614b300fb5c03b9c3f01ea79bfb03c5c41f
Mirko Geissler <mirko.geissler@artcom.de>
2019-09-12 11:14:24 +0200

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,17 @@ module.exports = (env = {}) => ({
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.(css)$/,
+        loader: "css-loader"
+      },
+      {
+        test: /\.(svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]"
+        }
       }
     ]
   },
