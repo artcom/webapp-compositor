8330ac691ff06d8078195c6743eb18bd88f5c433
Philipp Niederlag <philipp.niederlag@artcom.de>
2023-06-30 15:04:31 +0200

    Refert dev mode

--- src/index.js
+++ src/index.js
@@ -12,32 +12,26 @@ import "../css/main.css"
 const urlParams = new URLSearchParams(window.location.search)
 const showDebugControls = urlParams.get("debug") !== null
 
-if (!showDebugControls) {
-  const root = createRoot(document.getElementById("app"))
-  root.render(<div> HI</div>)
-} else {
-  // eslint-disable-next-line no-undef
-  const administrationTopic = process.env.ADMINISTRATION_TOPIC
+// eslint-disable-next-line no-undef
+const administrationTopic = process.env.ADMINISTRATION_TOPIC
 
-  if (!administrationTopic) {
-    console.error("Environment variable 'ADMINISTRATION_TOPIC' is missing")
-  }
-
-  const logger = createLogger({
-    collapsed: true,
-  })
-
-  const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, logger)(createStore)
-  const store = createStoreWithMiddleware(combineReducers(reducers))
-  const bootstrapData = Object.fromEntries(new URLSearchParams(window.location.search).entries())
-
-  createEventHandler(bootstrapData, administrationTopic, store)
-
-  const root = createRoot(document.getElementById("app"))
-  root.render(
-    <Provider store={store}>
-      {showDebugControls && <App showDebugControls={showDebugControls} />}
-      {!showDebugControls && <div> HI</div>}
-    </Provider>
-  )
+if (!administrationTopic) {
+  console.error("Environment variable 'ADMINISTRATION_TOPIC' is missing")
 }
+
+const logger = createLogger({
+  collapsed: true,
+})
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, logger)(createStore)
+const store = createStoreWithMiddleware(combineReducers(reducers))
+const bootstrapData = Object.fromEntries(new URLSearchParams(window.location.search).entries())
+
+createEventHandler(bootstrapData, administrationTopic, store)
+
+const root = createRoot(document.getElementById("app"))
+root.render(
+  <Provider store={store}>
+    <App showDebugControls={showDebugControls} />
+  </Provider>
+)
