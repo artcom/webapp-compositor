51b1c2fb3e246bfee6e5707a5db52844dc0656d5
Dino Lindlau <dino.lindlau@artcom.de>
2023-08-01 11:26:34 +0200

    Add redux toolkit, sind createStore is deprecated and change node to min

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { applyMiddleware, combineReducers, createStore } from "redux"
-import { createLogger } from "redux-logger"
-import thunkMiddleware from "redux-thunk"
-
 import App from "./components/app"
 import createEventHandler from "./deviceEventHandler"
-import * as reducers from "./reducers"
+import store from "./store"
 import "../css/main.css"
 
 const urlParams = new URLSearchParams(window.location.search)
 const showDebugControls = urlParams.get("debug") !== null
-
 const administrationTopic = process.env.ADMINISTRATION_TOPIC
 
 if (!administrationTopic) {
   console.error("Environment variable 'ADMINISTRATION_TOPIC' is missing")
 }
 
-const logger = createLogger({
-  collapsed: true,
-})
-
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, logger)(createStore)
-const store = createStoreWithMiddleware(combineReducers(reducers))
 const bootstrapData = Object.fromEntries(new URLSearchParams(window.location.search).entries())
 
 createEventHandler(bootstrapData, administrationTopic, store)
