870b2e60903ea1d7dd1df48f79ddede33d029ce4
Philipp Niederlag <philipp.niederlag@artcom.de>
2023-08-04 14:06:32 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import createEventHandler from "./deviceEventHandler"
 import store from "./store"
 import "../css/main.css"
 
-const urlParams = new URLSearchParams(window.location.search)
-const showDebugControls = urlParams.get("debug") !== null
-const administrationTopic = process.env.ADMINISTRATION_TOPIC
-
-if (!administrationTopic) {
-  console.error("Environment variable 'ADMINISTRATION_TOPIC' is missing")
-}
-
-const bootstrapData = Object.fromEntries(new URLSearchParams(window.location.search).entries())
-const { device, wsBrokerUri } = bootstrapData
-
 const start = async () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const showDebugControls = urlParams.get("debug") !== null
+  const administrationTopic = process.env.ADMINISTRATION_TOPIC
+
+  if (!administrationTopic) {
+    console.error("Environment variable 'ADMINISTRATION_TOPIC' is missing")
+  }
+
+  const bootstrapData = Object.fromEntries(new URLSearchParams(window.location.search).entries())
+  const { device, wsBrokerUri } = bootstrapData
+
   const mqttClient = await connectAsync(wsBrokerUri, {
     appId: "WebAppCompositor",
     deviceId: device,
